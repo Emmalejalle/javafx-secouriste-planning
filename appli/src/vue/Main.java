02bc5193b5859c822f1bd896c7df39c3ffa4e50f
emilien <emilemeriau@gmail.com>
2025-06-06 22:59:09 +0200

    Cette fois c'est la bonne

---
+++ appli/src/vue/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println(getClass().getResource("/vue/ProfileAdmin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/vue/Profil.fxml"));
+        stage.setTitle("Liste des secouristes");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
