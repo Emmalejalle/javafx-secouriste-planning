e1643e095940b11e3a00ef87e977787b57431ff1
emilien <emilemeriau@gmail.com>
2025-06-13 21:37:24 +0200

    ajout top bot corrigé et ajout top raccroché à la page AccueilAdmin + le fait d'adapter la taille de la fenêtre à l'appli automatiquement

--- appli/src/vue/MainEmilien.java
+++ appli/src/vue/MainEmilien.java
@@ -3,15 +3,46 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.transform.Scale;
+import javafx.beans.value.ObservableValue;
+
+// Importer les classes nécessaires pour l'application JavaFX
 
 public class MainEmilien extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println(getClass().getResource("/vue/ProfileAdmin.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/vue/ProfileAdmin.fxml"));
+        double width = 1920; // Largeur de la fenêtre initiale (prévue dans le fxml)
+        double height = 1080; // Hauteur de la fenêtre initiale (prévue dans le fxml)
+
+        // Charger le fichier FXML pour l'interface utilisateur
+        System.out.println(getClass().getResource("/vue/PatronHeaderAdmin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/vue/accueilAdmin.fxml"));
+
+        // Configurer la scène et le stage
+        Scene scene = new Scene(root);
+
+        // Définir le style de la fenêtre
         stage.setTitle("Liste des secouristes");
-        stage.setScene(new Scene(root));
+        stage.setScene(scene);
         stage.show();
+
+        // Changement de la taille de la l'application en fonction de la taille de la fenêtre
+
+        Scale scaleTransform = new Scale(1, 1);
+        root.getTransforms().add(scaleTransform);
+
+        // Listener sur la largeur de la fenêtre
+        scene.widthProperty().addListener((ObservableValue<? extends Number> obs, Number oldWidth, Number newWidth) -> {
+            double scaleX = newWidth.doubleValue() / width;
+            scaleTransform.setX(scaleX);
+        });
+
+        // Listener sur la hauteur de la fenêtre
+        scene.heightProperty().addListener((ObservableValue<? extends Number> obs, Number oldHeight, Number newHeight) -> {
+            double scaleY = newHeight.doubleValue() / height;
+            scaleTransform.setY(scaleY);
+        });
     }
 
     public static void main(String[] args) {
