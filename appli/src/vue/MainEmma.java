b55716cfb758905d1135f2c788b50532d3c954b5
Emma Le Jallé <Emma>
2025-06-16 23:30:43 +0200

    affect DPS

--- appli/src/vue/MainEmma.java
+++ appli/src/vue/MainEmma.java
@@ -8,7 +8,7 @@ public class MainEmma extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println(getClass().getResource("/vue/accueilAdmin.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/vue/accueilSecouriste.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/vue/AffectationVerifAutoAffect.fxml"));
         stage.setTitle("Liste des secouristes");
         stage.setScene(new Scene(root));
         stage.show();
